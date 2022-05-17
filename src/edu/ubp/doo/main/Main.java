a10fa95150baec093f977f16d128242cd1062962
SecondPort <gomezmanulm@gmail.com>
2022-05-16 22:05:13 -0300

    main

--- src/edu/ubp/doo/main/Main.java
+++ src/edu/ubp/doo/main/Main.java
@@ -1,7 +1,14 @@
 package edu.ubp.doo.main;
 
+import edu.ubp.doo.model.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Persona p = new Cliente("Juan", "Perez", "12345678", "Jubilado","goubat");
+        System.out.println(p);
     }
+
+
 }
